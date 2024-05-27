e3c49bbb98696c7149f53e76ea8dab5597368da3
TazoAbesadze4 <tazo.abesadze.4@gmail.com>
2024-05-27 10:07:20 -0700

    first commit

---
+++ day40/classwork/classwork.py
@@ -0,0 +1,36 @@
+# 1)
+# def sum_array(a):
+#     sum = 0
+    
+#     for i in a:
+#         sum += i
+        
+#     return sum
+
+
+# 2)
+# def find_average(numbers):
+#     if len(numbers) == 0:
+#         return 0
+#     average_of_numbers = 0
+    
+#     for i in numbers:
+#         average_of_numbers += i
+#     count = len(numbers)
+    
+#     return sum/count
+
+
+# 3)
+# def count_positives_sum_negatives(arr):
+#     if len(arr) == 0:
+#         return arr
+#     count = 0
+#     sum = 0
+#     for i in arr:
+#         if i > 0:
+#             count += 1
+#         else:
+#             sum += i
+            
+#     return (count , sum)
