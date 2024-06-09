ab6cfa50049cb126f64f14e0d6add45da3c69be6
TazoAbesadze4 <tazo.abesadze.4@gmail.com>
2024-06-09 03:09:15 -0700

    day

--- day02/homework/homework.py
+++ day02/homework/homework.py
@@ -3,6 +3,7 @@
 
 friends = "Dati Chxikvadze, Nika Kuchava, Luka Panqvelashvili" 
 met = "Gio Gagnidze, Nika Keshelava, Luka Cxvaradze, Demetre Ekseulidze, and more..."
+
 # what I am think about GOA?
 think = "გოა ვფიქრობ არის საუკეთესო აკადემია, რომელიც მჯერა რომ მიმაღწევინებს წარმატებებს"
 print(" I became friends with" + " " + friends + " i am also met" + " " + met + " i am think," + " "+think)
