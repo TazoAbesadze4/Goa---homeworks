7299deb998dd4a4b6b695b07c497afaf2968a497
TazoAbesadze4 <tazo.abesadze.4@gmail.com>
2024-08-03 03:28:10 -0700

    day

---
+++ day72/classwork/classwork.py
@@ -0,0 +1,13 @@
+# def pig_it(text):
+#     symbols = ['!', '?']
+#     words_list = text.split(" ")
+#     result = []
+    
+#     for word in words_list:
+#         if word not in symbols:
+#             result.append(word[1:] + word[0] + "ay")
+#         else:
+#             result.append(word)
+    
+#     return " ".join(result)
+
